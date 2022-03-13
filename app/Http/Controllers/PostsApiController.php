64eff41c648546df883da5b9f7fd0ed0d45a06ba
Rahaf Al-Jazzazi <83532535+RahafJ96@users.noreply.github.com>
2022-03-14 00:40:05 +0300

    test

---
+++ app/Http/Controllers/PostsApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostsApiController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+    public  function store(){
+
+        return Post::create([
+            'title'=> request('title'),
+            'content'=> request('content')
+        ]);
+    }
+
+    public function update(Post $post){
+        $success = $post->update([
+            'title'=>request('title'),
+            'content'=>request('content'),
+        ]);
+    
+        return [ 'success' => $success];
+    }
+
+    public function destroy(Post $post){
+        $success = $post ->delete();
+        return [ 'success' => $success];
+    }
+
+}
